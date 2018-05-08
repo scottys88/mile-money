9c64d1d1590f4779a5c0b1a56503dc39fb4bce7f
scottys88 <scott_schubert@hotmail.com.au>
2018-05-08 23:36:33 +0930

    Changed to exsting nodemon express server Set up schema for wishlist items Talking to database

--- myapp/controllers/dataController.js
+++ myapp/controllers/dataController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const wishlistItem = mongoose.model('wishlistItem');
 const strava = require('strava')({
         client_id: "22264",	
         client_secret: "f31774d980e2f6e97403b8fd404deecff420201a",
@@ -11,6 +13,16 @@ exports.homePage = (req, res) => {
   // 	return response;
   	res.render('index', { title: 'Shit some silly stuff' });
   };
+
+exports.addItem = (req, res) => {
+  res.render('addItem', {title: 'Add an Item'});
+};
+
+exports.saveItem = async (req, res) => {
+  const item = new wishlistItem(req.body);
+  await item.save();
+  res.redirect('/');
+};
 
 exports.scottStrava = (req, res) => {
   let stravaData = strava.athlete.get(function(err, stravaData) {
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const dataController = require('../controllers/dataController');
 const strava = require('strava')({
         client_id: "22264",	
@@ -7,6 +7,8 @@ const strava = require('strava')({
         redirect_uri: "http://localhost:3000/mile-money/",
         access_token: "a481290c9c01de67dbef70f0fb0b1207591c75cb"
 }); 
+
+const { catchErrors } = require('../handlers/errorHandlers')
 
 strava.athlete.get(function(err, res) {
         console.log(res);
@@ -16,8 +18,8 @@ strava.athlete.get(function(err, res) {
 
 /* GET home page. */
 router.get('/', dataController.homePage);
+router.get('/add-item', dataController.addItem);
+router.post('/add-item', catchErrors(dataController.saveItem));
 // router.get('/', dataController.scottStrava);
 
 module.exports = router;
-
-

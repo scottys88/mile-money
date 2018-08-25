09560cd257ad8c897db44f5a7cfe246a43e10af2
scottys88 <scott_schubert@hotmail.com.au>
2018-08-25 21:28:46 +0930

    icons and ripple effect working on the index.html page

--- login/src/app.js
+++ login/src/app.js
@@ -1,5 +1,6 @@
 import {MDCTemporaryDrawer} from '@material/drawer';
 import {MDCTopAppBar} from '@material/top-app-bar/index';
+import {MDCRipple} from '@material/ripple';
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
@@ -7,3 +8,6 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 const drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
 document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
+
+const surface = document.querySelectorAll('.my-surface');
+surface.forEach(item => new MDCRipple(item));

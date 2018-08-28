9b151a2053d1d922ddfc872eb25dbea840c45324
scottys88 <scott_schubert@hotmail.com.au>
2018-08-28 22:28:49 +0930

    Added commutes list layout

--- login/src/app.js
+++ login/src/app.js
@@ -15,3 +15,21 @@ surface.forEach(item => new MDCRipple(item));
 import {MDCTabBar} from '@material/tab-bar';
 
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+import {MDCChipSet} from '@material/chips';
+
+const chipSet = new MDCChipSet(document.querySelector('.mdc-chip'));
+
+
+//Expand and collapse the commute boxes
+
+const commuteExpand = document.querySelectorAll('.expand-commute');
+
+function expandCommute(e) {
+    const expandButton = e.currentTarget;
+    const visibleArea = expandButton.parentElement;
+    const expandableArea = visibleArea.nextElementSibling;
+    expandableArea.classList.toggle('expanded');
+}
+console.log('yo');
+commuteExpand.forEach(commute => commute.addEventListener('click', expandCommute));
